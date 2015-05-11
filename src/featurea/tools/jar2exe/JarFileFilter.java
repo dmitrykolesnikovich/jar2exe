52dc066000d744caaaed188570cb98c87c94724c
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2015-05-11 13:13:52 +0300

    initial commit

---
+++ src/featurea/tools/jar2exe/JarFileFilter.java
@@ -0,0 +1,18 @@
+package featurea.tools.jar2exe;
+
+import javax.swing.filechooser.FileFilter;
+import java.io.File;
+
+public  class JarFileFilter extends FileFilter {
+
+	@Override
+	public boolean accept(File file) {
+		return file.getAbsolutePath().endsWith(".jar") || file.isDirectory();
+	}
+
+	@Override
+	public String getDescription() {
+		return "Java Archives (*.jar)";
+	}
+
+}
---
+++ src/featurea/tools/jar2exe/Starter.java
@@ -0,0 +1,9 @@
+package featurea.tools.jar2exe;
+
+public class Starter {
+
+  public static void main(String[] args){
+    new Jar2ExeWindow();
+  }
+
+}
